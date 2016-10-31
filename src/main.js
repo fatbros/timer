86277f17fa926d828265aa296184b254dbec9f50
sh_sk <02luvsic@gmail.com>
2016-11-01 03:41:02 +0900

    add sidenav

--- src/main.js
+++ src/main.js
@@ -1,9 +1,12 @@
 import Vue from '../node_modules/vue/dist/vue.js';
+
+/**
+ * vue-materialを使用する
+ * https://marcosmoura.github.io/vue-material/#/
+ */
 import VueMaterial from 'vue-material'
 import '../node_modules/vue-material/dist/vue-material.css'
-
 Vue.use(VueMaterial);
-
 Vue.material.theme.registerAll({
     default: {
         primary: 'cyan',
@@ -15,9 +18,10 @@ Vue.material.theme.registerAll({
     }
 });
 
-const sidenav = require('./sidenav.vue');
-var app = new Vue(sidenav).$mount('#app');
+/**
+ * Vue Componentを定義する
+ */
+const sidenav = require('./component/sidenav/sidenav.vue');
+Vue.component('tm-sidenav', Vue.extend(sidenav));
 
-// new Vue({
-//     el: '#app'
-// });
+var app = new Vue().$mount('#app');
