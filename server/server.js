d75666c06438c757698e532de54fbb4239108043
sh_sk <02luvsic@gmail.com>
2016-11-01 19:48:50 +0900

    staticファイルパスの調整

--- server/server.js
+++ server/server.js
@@ -5,6 +5,9 @@ module.exports = {
     app: function () {
         const app = express()
         const indexPath = path.join(__dirname, '/../dest/index.html')
+        const publicPath = express.static(path.join(__dirname, '/../dest'))
+
+        app.use('/', publicPath)
         app.get('/', function (_, res) { res.sendFile(indexPath) })
 
         return app
