65126761045c988ed876b56bfdc889386c764bef
sh_sk <02luvsic@gmail.com>
2016-11-28 19:46:14 +0900

    経過時間を測るjs作成

---
+++ src/component/timer/timer.vue
@@ -0,0 +1,21 @@
+<template lang="html">
+    <div class="c-timer">
+        <p>
+            test
+        </p>
+    </div>
+</template>
+
+<script>
+// timer
+import Timer from '../../lib/timer.js';
+
+var t = new Timer();
+t.init();
+t.on('updatePast', (pastTime) => {
+    console.log(pastTime);
+});
+</script>
+
+<style lang="css">
+</style>
